763f47a357f7d6f6dd90fc170dac1e540dbab92e
Liliana <55440420+WhiteLilith@users.noreply.github.com>
2020-06-07 23:03:46 +0300

    Update 6.cpp

--- 10/6.cpp
+++ 10/6.cpp
@@ -7,16 +7,6 @@
 
 using namespace std;
 
-bMoney operator*(long double a, bMoney object)
-{
-	return bMoney(a * object.Value);
-}
-
-bMoney operator/(long double a, bMoney object)
-{
-	return bMoney(a / object.Value);
-}
-
 class bMoney
 {
 private:
@@ -26,9 +16,7 @@ private:
 	string money;
 	string digits = "0123456789";
 	long double Max = 9999999999999990.00;
-
 public:
-
 	bMoney(long double a)
 	{
 		if (a <= Max)
@@ -37,23 +25,19 @@ public:
 		}
 		else
 		{
-			cout << "Слишком большое значение." << endl;
+			cout << "ERROR, too big value";
 		}
 	}
-
 	bMoney()
 	{}
-
 	bMoney(char s[])
 	{
 		mstold(s);
 	}
-
 	bMoney(string s)
 	{
 		mstold(s);
 	}
-
 	bool Find(char c, string s)
 	{
 		bool result = false;
@@ -66,7 +50,6 @@ public:
 		}
 		return result;
 	}
-
 	void mstold(string s)
 	{
 		for (int i = 0; i < s.length(); i++)
@@ -77,14 +60,11 @@ public:
 				money.push_back(c);
 			}
 		}
-
 		char* numbersCh = new char[money.length() + 1];
 		strncpy_s(numbersCh, money.length()+1, money.c_str(), money.length());
-
 		Value = atof(numbersCh);
 		Value /= 100;
 	}
-
 	string ldtoms()
 	{
 		string uresult;
@@ -99,79 +79,69 @@ public:
 				result = " " + result;
 			}
 		}
-
 		result = Dollar + result;
 		Ustring = result;
 		return result;
 	}
-
 	bMoney operator+(bMoney a)
 	{
 		bMoney res(a.Value + Value);
 		return res;
 	}
-
 	bMoney operator-(bMoney a)
 	{
 		bMoney res(Value - a.Value);
 		return res;
 	}
-
 	bMoney operator*(long double a)
 	{
 		bMoney res(Value * a);
 		return res;
 	}
-
 	bMoney operator/(long double a)
 	{
 		bMoney res(Value / a);
 		return res;
 	}
-
 	long double operator/(bMoney a)
 	{
 		long double res = Value / a.Value;
 		return res;
 	}
-
 	friend bMoney operator*(long double, bMoney);
-
 	friend bMoney operator/(long double, bMoney);
-
 	operator long double()
 	{
 		return Value;
 	}
-
 	void getmoney()
 	{
-		cout << "Введите сумму: ";
+		cout << "Input the amount of money: ";
 		cin >> Value;
 	}
-
 	void putmoney()
-
 	{
 		cout << ldtoms();
 	}
-
 	void print_value()
 	{
 		cout << Value;
 	}
 };
-
+bMoney operator*(long double a, bMoney object)
+{
+	return bMoney(a * object.Value);
+}
+bMoney operator/(long double a, bMoney object)
+{
+	return bMoney(a / object.Value);
+}
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-
 	long double num = 150;
-
-	bMoney money(1.5);
-
-	cout << "/: " << num / money << endl;
-	cout << "*: " << num * money << endl;
-
+	bMoney money(2);
+	cout << num << " / " << money << " = " << num / money << endl;
+	cout << num << " * " << money << " = " << num * money << endl;
 	return 0;
 }
